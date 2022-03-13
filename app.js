09ded9d1f67d1a74f828162ba447af8ade6dee50
shahriyahosen <me.shahriyarhosen@gmail.com>
2022-03-14 00:54:01 +0600

    snippet

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log('first')
+console.log("first");
+// const name = value;
+c
+// let name = value;
+l
+// var name =value;
+v
+// else{
+
+// }
+e
+
+// if (condition) {
+
+// }
+// else if (condition) {
+
+// } 
+// ei
