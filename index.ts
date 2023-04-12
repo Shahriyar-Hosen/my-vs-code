096c81262da847dcc5e8d71f845913628442815e
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2023-04-12 11:11:11 +0600

    some thing added

---
+++ index.ts
@@ -0,0 +1,15 @@
+function fnName(params: string) {
+  const varOne = "value";
+  const varTwo = params;
+  const varThree = 11;
+
+  console.log(varOne, varTwo, varThree);
+}
+
+//  comment
+
+const fullName = (first: string, second: string) => {
+  return first + second;
+};
+
+fullName("a", "b");
